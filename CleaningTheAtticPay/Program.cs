900fe0b2409604f21886e3c187cba0e5d2a8ed65
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-20 14:22:45 -0500

    Cleaning The Attic

---
+++ CleaningTheAtticPay/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CleaningTheAtticPay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Last Friday Trevon had \$29. Over the weekend he received some money
+            //for cleaning the attic. He now has 41. How much money did he receive?
+
+            int had = 29;
+            int has = 41;
+            int attic = has - had;
+            Console.WriteLine("Trevon made $" + attic + " for cleaning the attic.");
+        }
+    }
+}
